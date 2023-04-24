04db27e5b430caa18d04ffbad093607d8773552f
Humblet Raphaël <raphum0182@gmail.com>
2023-04-24 08:43:02 +0200

    création de la db et compréhension de comment marche cette merde

--- sources/creation.py
+++ sources/creation.py
@@ -1,17 +1,45 @@
 import mysql.connector
 
 # Se connecter au serveur MySQL
-conn = mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="ZQ$G7ZWr;b|rD]B",
-    auth_plugin='mysql_native_password',
+    # password="ZQ$G7ZWr;b|rD]B",
+    passwd="root",
+    database="testdb"
 )
 
 
-print("Successfully connected to MySQL server")
+print("Successfully connected to MySQL database 'testdb'")
+
+# Créer le curseur qui permet de faire des queries et commandes SQL
+mycursor = db.cursor()
+
+# Créer une base de données (à ne faire qu'une fois la première fois qu'on run le code)
+
+# mycursor.execute("CREATE DATABASE testdb")
+# print("Successfully created database 'testdb'")
 
 
-# # Créer une base de données
-# cursor = conn.cursor()
-# cursor.execute("CREATE DATABASE ma_base_de_donnees")
+"""Basic queries and commands"""
+
+
+# Création d'une table avec 3 colonnes (name, age, personID)
+# mycursor.execute(
+#     "CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+# AUTO_INCREMENT permet d'incrémenter automatiquement la valeur de la colonne personID (éviter d'avoir 2 fois la même)
+
+
+# Ajout d'informations dans la table Person
+mycursor.execute(
+    "INSERT INTO Person (name, age) VALUES (%s, %s)", ("Tim", 91))
+db.commit()  # Commit les changements dans la base de données
+
+
+# Visualiser les données de la table Person
+mycursor.execute("SELECT * FROM Person")
+
+
+# mycursor.execute("DESCRIBE Person")
+for x in mycursor:
+    print(x)
