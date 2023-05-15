76430cadbc5e7cd0de20fbf1ed90923d9bca7f14
aachten <alexandre.achten@ulb.be>
2023-05-15 17:39:47 +0200

    correction requete 4

--- sources/Requetes/r4.sql
+++ sources/Requetes/r4.sql
@@ -2,5 +2,5 @@ SELECT distinct p.NISS, p.Lname, p.Fname
 FROM DossierPatient d
 JOIN Patient p ON d.NISSPatient = p.NISS
 JOIN Medicament m ON d.DCI = m.DCI
-WHERE m.NomCom = 'Acinax'
-AND d.dateVente > '1980-01-01';
+WHERE d.dateVente > '1980-01-01'
+AND d.NomComMedicament = 'Acinax';
