17f2e8ef7b9ee78c4f0f727625d74b5ae7eb4cc4
Nicolas <nicolas.guilbaud@ulb.be>
2023-05-26 10:22:31 +0200

    requête 7 complétée

--- sources/Requetes/r7.sql
+++ sources/Requetes/r7.sql
@@ -1,6 +1,12 @@
-
-SELECT m.`NomCom` AS nom, floor(YEAR(p.`Bdate`)/10)*10 AS decennie, COUNT(m.`DCI`) c
-        FROM patient p JOIN dossierpatient dossier ON dossier.`NISSPatient` = p.`NISS`
-        JOIN medicament m ON m.`DCI` = dossier.`DCI`
-        GROUP BY nom, decennie HAVING (decennie >= 1950 AND decennie < 2020)
-        ORDER BY decennie ASC;
+SELECT t2.nom, t2.decennie, t2.c
+FROM(
+        SELECT t1.nom, t1.decennie, t1.c, MAX(t1.c) OVER (PARTITION BY (t1.decennie)) AS m2
+        FROM (
+                SELECT m.`NomCom` AS nom, floor(YEAR(p.`Bdate`)/10)*10 AS decennie, COUNT(m.`DCI`) c
+                FROM patient p JOIN dossierpatient dossier ON dossier.`NISSPatient` = p.`NISS`
+                JOIN medicament m ON m.`DCI` = dossier.`DCI`
+                GROUP BY nom,decennie HAVING (decennie >= 1950 AND decennie < 2020)
+        ) AS t1
+) AS t2
+WHERE t2.c = t2.m2
+ORDER BY decennie ASC;
